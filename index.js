5138f2d061d722c1498ec45f072fb3590af347b9
Dhruv Jain <dhruvjainpenny@gmail.com>
2019-07-25 14:59:25 +0530

    reframed

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-const CheckWebWorker = require('check-web-workers-support')
-class WebWorker {
-  constructor (script) {
-    CheckWebWorker(() => {
-      console.log('Web Workers are supported')
-    })
-
-    if (!script) {
-      throw new Error('The script is required')
-    }
-    this._script = script
-    const code = this._script.toString()
-    const blob = new Blob(['(' + code + ')()'])
-    this._worker = new Worker(URL.createObjectURL(blob))
-    return this._worker
+module.exports = function (script) {
+  if (!script) {
+    throw new Error('The script is required')
   }
+  this._script = script
+  const code = this._script.toString()
+  const blob = new Blob(['(' + code + ')()'])
+  this._worker = new Worker(URL.createObjectURL(blob))
+  return this._worker
 }
-
-module.exports = WebWorker
